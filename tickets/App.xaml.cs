c2c53b51d18f8e051f4c62f587eb7c6522bea004
Wragdan <davidfernandomendez@gmail.com>
2018-08-11 23:05:03 -0600

    Added LoginPage

--- tickets/App.xaml.cs
+++ tickets/App.xaml.cs
@@ -11,7 +11,7 @@ namespace tickets
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
