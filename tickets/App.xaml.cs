a3c2fd42380752b8f03fb0456c7ecd3a88440dad
Wragdan <davidfernandomendez@gmail.com>
2018-08-14 01:46:05 -0600

    Added appcenter statistycs

--- tickets/App.xaml.cs
+++ tickets/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace tickets
@@ -11,12 +14,13 @@ namespace tickets
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=02cc353c-73aa-4d1e-a762-2025aeba8df7;" + "uwp={Your UWP App secret here};" + "android={Your Android App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
