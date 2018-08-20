493e9dff848f470a6221ec41434a57d795c3ef15
Wragdan <davidfernandomendez@gmail.com>
2018-08-20 17:22:12 -0600

    added tickets model

---
+++ tickets/Models/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using SQLite;
+
+namespace tickets.Models
+{
+    public class Ticket
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int UserID { get; set; }
+
+        public void PrintData()
+        {
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(this))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(this);
+                Console.WriteLine("{0}={1}", name, value);
+            }
+        }
+    }
+}
