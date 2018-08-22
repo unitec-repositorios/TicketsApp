677e96bcd3c7b24d20efb8d6c539422aaee32ef7
Wragdan <davidfernandomendez@gmail.com>
2018-08-22 00:49:41 -0600

    finished view account settings

--- tickets/screens/UserSettingsPage.xaml.cs
+++ tickets/screens/UserSettingsPage.xaml.cs
@@ -11,5 +11,29 @@ namespace tickets
         {
             InitializeComponent();
         }
+
+        async protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            User current = await App.Database.GetCurrentUser();
+            if (current == null) {
+                BindingContext = new User();
+            } else {
+                BindingContext = current;
+            }
+        }
+
+        void OnSaveTouched(object sender, System.EventArgs e)
+        {
+            var user = (User)BindingContext;
+            user.PrintData();
+            Console.WriteLine();
+            App.Database.SaveUserAsync(user);
+        }
+
+        void OnClearTouched(object sender, System.EventArgs e)
+        {
+            BindingContext = new User();
+        }
     }
 }
