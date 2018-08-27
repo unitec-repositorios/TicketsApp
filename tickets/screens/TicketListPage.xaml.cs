6d5febb23eaa69a7edbc5c4437d7cde092bd0166
Wragdan <davidfernandomendez@gmail.com>
2018-08-27 17:54:33 -0600

    added tickets list, app settings

---
+++ tickets/screens/TicketListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace tickets.screens
+{
+    public partial class TicketListPage : ContentPage
+    {
+        public TicketListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
