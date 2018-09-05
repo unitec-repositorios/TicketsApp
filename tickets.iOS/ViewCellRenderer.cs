4440b27328c04315640980d9c74e1c34bad0300a
Wragdan <davidfernandomendez@gmail.com>
2018-09-05 01:06:11 -0600

    changed default cell renderer for ios

--- tickets.iOS/ViewCellRenderer.cs
+++ tickets.iOS/ViewCellRenderer.cs
@@ -8,28 +8,30 @@ namespace tickets.iOS
     public class StandardViewCellRenderer : ViewCellRenderer
     {
 
-        public override UIKit.UITableViewCell GetCell (Cell item, UIKit.UITableViewCell reusableCell, UIKit.UITableView tv)
+        public override UIKit.UITableViewCell GetCell(Cell item, UIKit.UITableViewCell reusableCell, UIKit.UITableView tv)
         {
-            var cell = base.GetCell (item, reusableCell, tv);
-                    switch (item.StyleId)
-                    {
-                        case "none":
-                            cell.Accessory = UIKit.UITableViewCellAccessory.None;
-                            break;
-                        case "checkmark":
-                            cell.Accessory = UIKit.UITableViewCellAccessory.Checkmark;
-                            break;
-                        case "detail-button":
-                            cell.Accessory = UIKit.UITableViewCellAccessory.DetailButton;
-                            break;
-                        case "detail-disclosure-button":
-                            cell.Accessory = UIKit.UITableViewCellAccessory.DetailDisclosureButton;
-                            break;
-                        case "disclosure":
-                        default:
-                            cell.Accessory = UIKit.UITableViewCellAccessory.DisclosureIndicator;
-                            break;
-                    }
+            var cell = base.GetCell(item, reusableCell, tv);
+            switch (item.StyleId)
+            {
+                case "none":
+                    cell.Accessory = UIKit.UITableViewCellAccessory.None;
+                    break;
+                case "checkmark":
+                    cell.Accessory = UIKit.UITableViewCellAccessory.Checkmark;
+                    break;
+                case "detail-button":
+                    cell.Accessory = UIKit.UITableViewCellAccessory.DetailButton;
+                    break;
+                case "detail-disclosure-button":
+                    cell.Accessory = UIKit.UITableViewCellAccessory.DetailDisclosureButton;
+                    break;
+                case "disclosure":
+                    cell.Accessory = UIKit.UITableViewCellAccessory.DisclosureIndicator;
+                    break;
+                default:
+                    cell.Accessory = UIKit.UITableViewCellAccessory.None;
+                    break;
+            }
             return cell;
         }
 
