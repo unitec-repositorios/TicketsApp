368992e6497b5bfef56c70dc1885fa8353751f1b
sborjas <iborjas@unitec.edu>
2018-10-31 12:59:02 -0600

    pequenos detalles

--- TicketsApp-develop/tickets.Android/Resources/Resource.Designer.cs
+++ TicketsApp-develop/tickets.Android/Resources/Resource.Designer.cs
@@ -3868,26 +3868,26 @@ namespace tickets.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013f
-			public const int avd_hide_password_1 = 2130837823;
-			
 			// aapt resource value: 0x7f020140
-			public const int avd_hide_password_2 = 2130837824;
+			public const int avd_hide_password_1 = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int avd_hide_password_3 = 2130837825;
+			public const int avd_hide_password_2 = 2130837825;
+			
+			// aapt resource value: 0x7f020142
+			public const int avd_hide_password_3 = 2130837826;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020142
-			public const int avd_show_password_1 = 2130837826;
-			
 			// aapt resource value: 0x7f020143
-			public const int avd_show_password_2 = 2130837827;
+			public const int avd_show_password_1 = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int avd_show_password_3 = 2130837828;
+			public const int avd_show_password_2 = 2130837828;
+			
+			// aapt resource value: 0x7f020145
+			public const int avd_show_password_3 = 2130837829;
 			
 			// aapt resource value: 0x7f020056
 			public const int baseline_camera_alt_white_18 = 2130837590;
@@ -4561,11 +4561,11 @@ namespace tickets.Droid
 			// aapt resource value: 0x7f020135
 			public const int notification_icon_background = 2130837813;
 			
-			// aapt resource value: 0x7f02013d
-			public const int notification_template_icon_bg = 2130837821;
-			
 			// aapt resource value: 0x7f02013e
-			public const int notification_template_icon_low_bg = 2130837822;
+			public const int notification_template_icon_bg = 2130837822;
+			
+			// aapt resource value: 0x7f02013f
+			public const int notification_template_icon_low_bg = 2130837823;
 			
 			// aapt resource value: 0x7f020136
 			public const int notification_tile_bg = 2130837814;
@@ -4577,16 +4577,19 @@ namespace tickets.Droid
 			public const int plus1 = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int sendIcon = 2130837817;
+			public const int send1 = 2130837817;
 			
 			// aapt resource value: 0x7f02013a
-			public const int settings_icon = 2130837818;
+			public const int sendIcon = 2130837818;
 			
 			// aapt resource value: 0x7f02013b
-			public const int tooltip_frame_dark = 2130837819;
+			public const int settings_icon = 2130837819;
 			
 			// aapt resource value: 0x7f02013c
-			public const int tooltip_frame_light = 2130837820;
+			public const int tooltip_frame_dark = 2130837820;
+			
+			// aapt resource value: 0x7f02013d
+			public const int tooltip_frame_light = 2130837821;
 			
 			static Drawable()
 			{
