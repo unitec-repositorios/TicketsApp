1b23969b1db013b27beb981a128e9df7337077cc
Andy Escobar <andyescobar007@gmail.com>
2020-03-12 17:35:05 -0600

    Done!

---
+++ tickets/CustomControls/XEntryRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tickets.CustomControls;
+using Xamarin.Forms;
+
+
+namespace tickets.CustomControls
+{
+    public class XEntryRenderer
+    {
+
+    }
+}
---
+++ tickets/Views/ChatTicketView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tickets.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatTicketView : ContentPage
+    {
+        public ChatTicketView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (EditorText.Text.Length == 10)
+            {
+                MessageContainer.HeightRequest += 20;
+            }else if (EditorText.Text.Length == 20)
+            {
+                MessageContainer.HeightRequest += 20;
+            }else if (EditorText.Text.Length < 10)
+            {
+                MessageContainer.HeightRequest = 32;
+            }
+        }
+    }
+}
